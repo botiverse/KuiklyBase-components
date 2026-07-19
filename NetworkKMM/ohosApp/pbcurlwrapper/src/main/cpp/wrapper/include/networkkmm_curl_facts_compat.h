154476164423bea02dfe079d4c28b38613144ec3
Quinn Rafferty <raft-mobile-cc-xile@mail.build>
2026-07-19 16:25:29 +0000

    fix(network): resolve optional iOS curl symbols dynamically
    Signed-off-by: Quinn Rafferty <raft-mobile-cc-xile@mail.build>

--- NetworkKMM/ohosApp/pbcurlwrapper/src/main/cpp/wrapper/include/networkkmm_curl_facts_compat.h
+++ NetworkKMM/ohosApp/pbcurlwrapper/src/main/cpp/wrapper/include/networkkmm_curl_facts_compat.h
@@ -3,28 +3,22 @@
 
 #include "curl_wrapper.h"
 
+#if defined(__APPLE__)
+#include <dlfcn.h>
+#endif
+
 // Native apps can be rebuilt before their packaged wrapper artifact is. Keep
 // the additive facts symbol weak so an older artifact remains request-capable:
 // callers receive unavailable facts instead of a load/link failure.
-#if defined(__APPLE__)
-extern int GetCurlTransferInfoV1(CurClientHandle handle, CurlTransferInfoV1 *info,
-                                 size_t infoSize, int abiVersion)
-    __attribute__((weak_import));
-#elif defined(__GNUC__)
+#if !defined(__APPLE__) && defined(__GNUC__)
 #pragma weak GetCurlTransferInfoV1
 #endif
 
-#if defined(__APPLE__)
-extern void SetCurlMaxBufferedResponseBytes(CurClientHandle handle, int64_t maxBytes)
-    __attribute__((weak_import));
-#elif defined(__GNUC__)
+#if !defined(__APPLE__) && defined(__GNUC__)
 #pragma weak SetCurlMaxBufferedResponseBytes
 #endif
 
-#if defined(__APPLE__)
-extern void SetCurlBufferedBodyIdleTimeoutMs(CurClientHandle handle, int64_t timeoutMs)
-    __attribute__((weak_import));
-#elif defined(__GNUC__)
+#if !defined(__APPLE__) && defined(__GNUC__)
 #pragma weak SetCurlBufferedBodyIdleTimeoutMs
 #endif
 
@@ -34,32 +28,71 @@ static inline int NetworkKmmGetCurlTransferInfoV1IfAvailable(
     size_t infoSize,
     int abiVersion
 ) {
+#if defined(__APPLE__)
+    typedef int (*GetTransferInfoFn)(
+        CurClientHandle,
+        CurlTransferInfoV1 *,
+        size_t,
+        int);
+    GetTransferInfoFn function = (GetTransferInfoFn)dlsym(
+        RTLD_DEFAULT,
+        "GetCurlTransferInfoV1");
+    if (function == 0) {
+        return 0;
+    }
+    return function(handle, info, infoSize, abiVersion);
+#else
     if (GetCurlTransferInfoV1 == 0) {
         return 0;
     }
     return GetCurlTransferInfoV1(handle, info, infoSize, abiVersion);
+#endif
 }
 
 static inline int NetworkKmmSetCurlMaxBufferedResponseBytesIfAvailable(
     CurClientHandle handle,
     int64_t maxBytes
 ) {
+#if defined(__APPLE__)
+    typedef void (*SetMaxBufferedResponseBytesFn)(CurClientHandle, int64_t);
+    SetMaxBufferedResponseBytesFn function = (SetMaxBufferedResponseBytesFn)dlsym(
+        RTLD_DEFAULT,
+        "SetCurlMaxBufferedResponseBytes");
+    if (function == 0) {
+        return 0;
+    }
+    function(handle, maxBytes);
+    return 1;
+#else
     if (SetCurlMaxBufferedResponseBytes == 0) {
         return 0;
     }
     SetCurlMaxBufferedResponseBytes(handle, maxBytes);
     return 1;
+#endif
 }
 
 static inline int NetworkKmmSetCurlBufferedBodyIdleTimeoutMsIfAvailable(
     CurClientHandle handle,
     int64_t timeoutMs
 ) {
+#if defined(__APPLE__)
+    typedef void (*SetBufferedBodyIdleTimeoutMsFn)(CurClientHandle, int64_t);
+    SetBufferedBodyIdleTimeoutMsFn function = (SetBufferedBodyIdleTimeoutMsFn)dlsym(
+        RTLD_DEFAULT,
+        "SetCurlBufferedBodyIdleTimeoutMs");
+    if (function == 0) {
+        return 0;
+    }
+    function(handle, timeoutMs);
+    return 1;
+#else
     if (SetCurlBufferedBodyIdleTimeoutMs == 0) {
         return 0;
     }
     SetCurlBufferedBodyIdleTimeoutMs(handle, timeoutMs);
     return 1;
+#endif
 }
 
 #endif  // NETWORKKMM_CURL_FACTS_COMPAT_H
