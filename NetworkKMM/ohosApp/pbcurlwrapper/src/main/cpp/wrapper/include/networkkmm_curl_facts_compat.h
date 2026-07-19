ee05ac426d763ced8c792eb6fd99408f6ce6f092
CC-希乐 <raft-mobile-cc-xile@mail.build>
2026-07-19 19:58:39 +0000

    feat(network): add optional multi bridge helpers
    Signed-off-by: CC-希乐 <raft-mobile-cc-xile@mail.build>

--- NetworkKMM/ohosApp/pbcurlwrapper/src/main/cpp/wrapper/include/networkkmm_curl_facts_compat.h
+++ NetworkKMM/ohosApp/pbcurlwrapper/src/main/cpp/wrapper/include/networkkmm_curl_facts_compat.h
@@ -20,7 +20,87 @@
 
 #if !defined(__APPLE__) && defined(__GNUC__)
 #pragma weak SetCurlBufferedBodyIdleTimeoutMs
+#pragma weak CreateCurlMultiEngine
+#pragma weak SubmitBufferedRequestV27
+#pragma weak CancelCurlMultiRequest
+#pragma weak GetCurlMultiInfoV1
 #endif
+
+static inline int NetworkKmmCurlMultiApiAvailable(void) {
+#if defined(__APPLE__)
+    return dlsym(RTLD_DEFAULT, "CreateCurlMultiEngine") != 0 &&
+        dlsym(RTLD_DEFAULT, "SubmitBufferedRequestV27") != 0 &&
+        dlsym(RTLD_DEFAULT, "CancelCurlMultiRequest") != 0 &&
+        dlsym(RTLD_DEFAULT, "GetCurlMultiInfoV1") != 0;
+#else
+    return CreateCurlMultiEngine != 0 &&
+        SubmitBufferedRequestV27 != 0 &&
+        CancelCurlMultiRequest != 0 &&
+        GetCurlMultiInfoV1 != 0;
+#endif
+}
+
+static inline CurlMultiEngineHandle NetworkKmmCreateCurlMultiEngineIfAvailable(
+    const char *logTag
+) {
+#if defined(__APPLE__)
+    typedef CurlMultiEngineHandle (*CreateFn)(const char *);
+    CreateFn function = (CreateFn)dlsym(RTLD_DEFAULT, "CreateCurlMultiEngine");
+    return function == 0 ? 0 : function(logTag);
+#else
+    return CreateCurlMultiEngine == 0 ? 0 : CreateCurlMultiEngine(logTag);
+#endif
+}
+
+static inline int NetworkKmmSubmitBufferedRequestV27IfAvailable(
+    CurlMultiEngineHandle engine,
+    int64_t requestId,
+    CurClientHandle handle,
+    const CurlRequest *request,
+    size_t requestSize,
+    int abiVersion,
+    const CurlCallback *callback
+) {
+#if defined(__APPLE__)
+    typedef int (*SubmitFn)(CurlMultiEngineHandle, int64_t, CurClientHandle,
+        const CurlRequest *, size_t, int, const CurlCallback *);
+    SubmitFn function = (SubmitFn)dlsym(RTLD_DEFAULT, "SubmitBufferedRequestV27");
+    return function == 0 ? 0 : function(
+        engine, requestId, handle, request, requestSize, abiVersion, callback);
+#else
+    return SubmitBufferedRequestV27 == 0 ? 0 : SubmitBufferedRequestV27(
+        engine, requestId, handle, request, requestSize, abiVersion, callback);
+#endif
+}
+
+static inline void NetworkKmmCancelCurlMultiRequestIfAvailable(
+    CurlMultiEngineHandle engine,
+    int64_t requestId
+) {
+#if defined(__APPLE__)
+    typedef void (*CancelFn)(CurlMultiEngineHandle, int64_t);
+    CancelFn function = (CancelFn)dlsym(RTLD_DEFAULT, "CancelCurlMultiRequest");
+    if (function != 0) function(engine, requestId);
+#else
+    if (CancelCurlMultiRequest != 0) CancelCurlMultiRequest(engine, requestId);
+#endif
+}
+
+static inline int NetworkKmmGetCurlMultiInfoV1IfAvailable(
+    CurClientHandle handle,
+    CurlMultiInfoV1 *info,
+    size_t infoSize,
+    int abiVersion
+) {
+#if defined(__APPLE__)
+    typedef int (*GetInfoFn)(CurClientHandle, CurlMultiInfoV1 *, size_t, int);
+    GetInfoFn function = (GetInfoFn)dlsym(RTLD_DEFAULT, "GetCurlMultiInfoV1");
+    return function == 0 ? 0 : function(handle, info, infoSize, abiVersion);
+#else
+    return GetCurlMultiInfoV1 == 0 ? 0 :
+        GetCurlMultiInfoV1(handle, info, infoSize, abiVersion);
+#endif
+}
 
 static inline int NetworkKmmGetCurlTransferInfoV1IfAvailable(
     CurClientHandle handle,
